c9ddc99b1d461531e83bb25ed8ff22a2882a68a4
sajidmahamud835 <sajidmahamud835@gmail.com>
2022-04-06 08:51:59 +0600

    button added, show false result

--- js/app.js
+++ js/app.js
@@ -44,17 +44,44 @@ search = (roll) => {
 
 showResult = (firstName, lastName, roll, college) => {
     console.log(firstName, lastName, roll, college)
-    document.getElementById("result").innerHTML = `
+    if (college != false) {
+        document.getElementById("result").innerHTML = `
         <div>
             <h1 class="text-center text-primary">Congratulation ${firstName + " " + lastName}!</h1>
             <p class="text-center"> You got selected for <span class="text-info"> ${college} </span>.</p>
             <div class="d-flex">
-            <img width="400px" height="320px" class="w-50 mx-auto my-3"
-                src="https://cdn.pixabay.com/photo/2016/04/01/09/51/boy-1299626_960_720.png" alt=""
-                srcset="">
+                <img width="400px" height="400px" class="w-50 mx-auto my-3"
+                    src="https://cdn.pixabay.com/photo/2016/04/01/09/51/boy-1299626_960_720.png" alt=""
+                    srcset="">
+            </div>
+            <a onclick="handelViewBtn()" class="btn btn-danger m-2" id="btn" type="submit">View Merit Position & Marks</a>
         </div>
+    `;
+    }
+    else {
+        document.getElementById("result").innerHTML = `
+        <div>
+            <h1 class="text-center text-primary">Sorry ${firstName + " " + lastName}!</h1>
+            <p class="text-center text-danger"> Unfortunately, you haven't got selected for any government medical college.</p>
+            <div class="d-flex">
+                 <img width="400px" height="400px" class="w-50 mx-auto my-3"
+                src="https://cdn.pixabay.com/photo/2021/11/20/03/38/girl-6810780_960_720.png" alt=""
+                srcset="">
+            </div>
+            <a onclick="handelViewBtn()" class="btn btn-success m-2" id="btn" type="submit">View Merit Position & Marks</a>
+            <a onclick="handelDownloadBtn()" class="btn btn-danger m-2" id="btn" type="submit">PDF Merit List</a>
         </div>
     `
+    }
+
+}
+
+handelViewBtn = () => {
+    window.location.href = "https://result.dghs.gov.bd/mbbs/";
+}
+
+handelDownloadBtn = () => {
+    window.location.href = "https://old.dghs.gov.bd/images/docs/Notice/notice_5_4_2022_MBBS.pdf";
 }
 
 searchHandeler = (firstName, lastName, roll) => {
